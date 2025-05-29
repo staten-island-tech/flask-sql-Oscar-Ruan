1153470d56b33f29e16f32f9ebbc52404af2bf4b
Oscar-Ruan <oscarr193@nycstudents.net>
2025-05-29 08:57:37 -0400

    sql

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for, abort
 import requests
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bookings.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
